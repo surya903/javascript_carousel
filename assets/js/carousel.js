938170779832b5da74972790b0622cd5719cfd80
surya903 <suryasurya903@gmail.com>
2021-04-13 13:02:54 +0530

    autoplay added

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -2,13 +2,32 @@
     let getItems = document.querySelectorAll(".s_carousel .item");
     let prevItem = document.querySelector(".s_carousel .prev");
     let nextItem = document.querySelector(".s_carousel .next");
+    let timer = 0;
+    const autoplay = false;
+    if(autoplay){
+        setInterval(()=>{
+            next();
+        },1000);
+    }
 
-    
-
-    prevItem.addEventListener('click', function(){
-        
-    });
-    nextItem.addEventListener('click', function(){
+    const prev =  () => {
+   
+        //store the current active element in variable
+        const currentActive = document.querySelector(".s_carousel .active");
+        //remove the active class for the element
+        currentActive.classList.remove("active");
+        //check if the current active element has previous element 
+        // if it has previous element add the active class to the previous element 
+        // or add the active class to the last element
+        if(currentActive.previousElementSibling){
+            currentActive.previousElementSibling.classList.add("active");
+        }else{
+            getItems[getItems.length-1].classList.add("active");
+        }
+            
+    }
+     
+    const next = ()=>{
         //store the current active element in variable
         const currentActive = document.querySelector(".s_carousel .active");
         //remove the active class for the element
@@ -21,7 +40,11 @@
         } else{
             getItems[0].classList.add('active');
         }
-        
-    });
+
+    }
+
+
+    prevItem.addEventListener('click',  prev);
+    nextItem.addEventListener('click', next);
     
 })();
